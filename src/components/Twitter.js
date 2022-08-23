92be4d5f8f4610b4443b46989d6db621d12003e6
jamil <jamilkhan.nu@gmail.com>
2022-08-23 01:27:50 +0100

    twitter signin page complete

--- src/components/Twitter.js
+++ src/components/Twitter.js
@@ -17,32 +17,23 @@ const Twitter = () => {
     <Container
       sx={{
         mt: 5,
+
         display: "flex",
         flexDirection: "row",
         justifyContent: "center",
       }}
     >
-      <Box border={1}>
+      <Box border={1} sx={{ width: "450px", height: "450px", borderRadius: 3 }}>
         <Box
           sx={{
+            pt: 1,
             display: "flex",
             flexDirection: "row",
             justifyContent: "center",
+            color: "#42a5f5",
           }}
         >
-          <FaTwitter size={30} />
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
-            mt: 2,
-          }}
-        >
-          <Typography variant="h5" fontWeight="bold">
-            Sign in to Twitter
-          </Typography>
+          <FaTwitter size={25} />
         </Box>
         <Box>
           <Box
@@ -50,79 +41,138 @@ const Twitter = () => {
               display: "flex",
               flexDirection: "row",
               justifyContent: "center",
-
-              borderRadius: 10,
-              mt: 2,
+              pt: 3,
             }}
           >
-            <Button variant="outlined" sx={{ gap: 1, borderRadius: 10 }}>
-              <AiFillApple size={20} />
-              <Typography sx={{ textTransform: "capitalize" }}>
-                Continue with Apple
-              </Typography>
-            </Button>
+            <Typography variant="h5" fontWeight="bold">
+              Sign in to Twitter
+            </Typography>
           </Box>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
+          <Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                borderRadius: 10,
+                borderColor: "#bdbdbd",
+                pt: 4,
+              }}
+            >
+              <Button
+                variant="outlined"
+                sx={{
+                  borderColor: "#bdbdbd",
+                  color: "black",
+                  pl: 5,
+                  pr: 5,
+                  gap: 1,
+                  borderRadius: 10,
+                }}
+              >
+                <AiFillApple size={20} />
+                <Typography
+                  variant="subtitle2"
+                  fontWeight="bold"
+                  sx={{ textTransform: "capitalize" }}
+                >
+                  Continue with Apple
+                </Typography>
+              </Button>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                borderColor: "#bdbdbd",
+                borderRadius: 10,
+                mt: 1,
+              }}
+            >
+              <Button
+                variant="outlined"
+                sx={{
+                  borderColor: "#bdbdbd",
+                  color: "black",
+                  pl: 5,
+                  pr: 5,
+                  gap: 1,
+                  borderRadius: 10,
+                }}
+              >
+                <AiOutlineGoogle size={20} />
+                <Typography
+                  variant="subtitle2"
+                  fontWeight="bold"
+                  sx={{ textTransform: "capitalize" }}
+                >
+                  Continue with Google
+                </Typography>
+              </Button>
+            </Box>
+            <Divider>or</Divider>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+              }}
+            >
+              <TextField
+                id="outlined-basic"
+                label="Phone, email, username"
+                variant="outlined"
+                sx={{ width: "15rem" }}
+              />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
 
-              borderRadius: 10,
-              mt: 1,
-            }}
-          >
-            <Button variant="outlined" sx={{ gap: 1, borderRadius: 10 }}>
-              <AiOutlineGoogle size={20} />
-              <Typography sx={{ textTransform: "capitalize" }}>
-                Continue with Google
-              </Typography>
-            </Button>
-          </Box>
-          <Divider>or</Divider>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-            }}
-          >
-            <TextField
-              id="outlined-basic"
-              label="Outlined"
-              variant="outlined"
-            />
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-
-              borderRadius: 10,
-              mt: 1,
-            }}
-          >
-            <Button variant="contained" sx={{ borderRadius: 10 }}>
-              <Typography sx={{ textTransform: "capitalize" }}>Next</Typography>
-            </Button>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-
-              borderRadius: 10,
-              mt: 1,
-              mb: 2,
-            }}
-          >
-            <Button variant="outlined" sx={{ borderRadius: 10 }}>
-              <Typography sx={{ textTransform: "capitalize" }}>
-                Forgot password?
-              </Typography>
-            </Button>
+                borderRadius: 10,
+                mt: 2,
+              }}
+            >
+              <Button
+                variant="contained"
+                sx={{ bgcolor: "black", pl: 14, pr: 14, borderRadius: 10 }}
+              >
+                <Typography
+                  variant="subtitle2"
+                  fontWeight="bold"
+                  sx={{ textTransform: "capitalize" }}
+                >
+                  Next
+                </Typography>
+              </Button>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                borderRadius: 10,
+                mt: 2,
+                mb: 2,
+              }}
+            >
+              <Button
+                variant="outlined"
+                sx={{ borderColor: "#bdbdbd", pl: 8, pr: 8, borderRadius: 10 }}
+              >
+                <Typography
+                  color="black"
+                  variant="subtitle2"
+                  fontWeight="bold"
+                  sx={{ textTransform: "capitalize" }}
+                >
+                  Forgot password?
+                </Typography>
+              </Button>
+            </Box>
           </Box>
         </Box>
       </Box>
